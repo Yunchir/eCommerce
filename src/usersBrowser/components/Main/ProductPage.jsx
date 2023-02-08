18853dcbcd5aa31b18060c6ce53fa409517822ef
Дамбийням У <dambii5655@gmail.com>
2023-02-09 07:40:49 +0800

    product

--- src/usersBrowser/components/Main/ProductPage.jsx
+++ src/usersBrowser/components/Main/ProductPage.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { data } from "../../../util/data";
+import "../../style/main/productPage.css";
 
 export default function ProductCard() {
   const test = useParams();
@@ -7,18 +8,36 @@ export default function ProductCard() {
   return (
     <div>
       <div>
-        <div>
-          <div>
+        <div className="proAdd">
+          <div className="proAdd-img">
             <img src={dat[0].image} alt="" />
           </div>
           <div className="proinfo">
             <h3>{dat[0].name}</h3>
-            <p>{dat[0].category}</p>
+            <p>${dat[0].price}</p>
+            <p>
+              {dat[0].category}: <span className="inStock">In stock</span>
+            </p>
+            <span className="hurryUp">
+              Hurry up! only 34 product left in stock
+            </span>
+            <span className="proColor">
+              <p>Color:</p>
+              <div className="proColors">
+                <div className="proColorNeon"></div>
+                <div className="procolorBlue"></div>
+              </div>
+            </span>
+            <div>
+              <span>Size:</span>
+              <button>23</button>
+              <button>34</button>
+              <button>13</button>
+              <button>24</button>
+            </div>
             <p>{dat[0].description}</p>
             {/* <p>{dat[0].spec}</p> */}
-            <div className="proprice">
-              <p>${dat[0].price}</p>
-            </div>
+            <div className="proprice"></div>
             <a href="../User.jsx">back</a>
           </div>
         </div>
